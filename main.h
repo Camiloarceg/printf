67f9c90d5b1f19f538c168c25f94e3743fa47578
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-11-09 17:19:40 +0000

    betty fixed for main.h

--- main.h
+++ main.h
@@ -8,5 +8,6 @@ char *neststring(char *buffer, char *s, int *count, int *i);
 char *nest_int(char *buffer, int *count, int *i, int n);
 int _strlen(char *s);
 char *neststring_r(char *buffer, char *s, int *count, int *i);
-char *switch_case_adv(char *buffer, const char *format, int *count, va_list ap, int *i);
+char *switch_case_adv(char *buffer, const char *format, int *count,
+		va_list ap, int *i);
 #endif
